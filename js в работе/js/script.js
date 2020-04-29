8f4e5f39337440f9420c52c22c9507d531dae159
Alina <alinachervonec.98@gmail.com>
2020-04-30 01:46:19 +0300

    задание 11

--- js в работе/js/script.js
+++ js в работе/js/script.js
@@ -84,4 +84,33 @@ window.addEventListener('DOMContentLoaded', function () {
     };
 
     setClock('timer', deadline);
+
+    //Модальное окно
+
+    let more = document.querySelector('.more');
+    let overlay = document.querySelector('.overlay');
+    let close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    //Еще одно 
+
+    let tabBtns = document.querySelectorAll('.description-btn');
+
+    tabBtns.forEach(function(item, i, tabBtns) {
+        item.addEventListener('click', function() {
+            overlay.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    })
 })
